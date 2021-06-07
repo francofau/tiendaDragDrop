b68b49cdb0b77ef2a6553afe2059eeb2ceee7542
mverikas <martinverikas@hotmail.com>
2021-06-07 11:45:35 +0200

    update tabla

--- js/script.js
+++ js/script.js
@@ -8,43 +8,48 @@ productos = [
   ["Pimientos", 2, 1]
 ];
 
+var productsSelectors = [
+  document.querySelector("#Patatas"), document.querySelector("#Zanahorias")
+];
+
 // Reacciones Botones
 var addToCartButtons = document.querySelectorAll(".addCarrito");
 addToCartButtons.forEach((addToCart) => {
   addToCart.addEventListener("click", () => {
-  console.log("click");
+  console.log(productsSelectors);
   });
 });
 
 //Creacion de tabla para el carrito
 var tabla = document.createElement("table");
 document.querySelector("#lista").appendChild(tabla);
-
+var indicadores = document.createElement("tr");
+var tablasIndicadores = `<th>Producto</th><th>Precio</th><th>Unidades</th>`;
+indicadores.innerHTML = tablasIndicadores;
+tabla.appendChild(indicadores);
+tabla.id = "tabla";
+tabla.style.display = "none"
 
 // Permiso para Drag
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
-// Evento Drag
+// En el Drag
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-// Evento Drop
+// En el Drop
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   console.log(data);
 
-  /* Crear las tablas indicadoras
-  var indicadores = document.createElement("tr");
-  var tablasIndicadores = "<th>Producto</th><th>Precio</th><th>Unidades</th>";
-  indicadores.appendChild(tablasIndicadores);
-*/
+  // Display ON de Indicadores
+  tabla.style.display = "table"
   // Generador de la Tabla del Carrito 
   for (let producto of productos) {
-    //tabla.appendChild(indicadores);
     var tr = document.createElement("tr");
     var ifExistData = document.querySelector(`#tr${producto[0]}`);
     if (producto[0] == data) {
