1eb8e47c3e4c955098fd634de8e313a56532706b
mverikas <martinverikas@hotmail.com>
2021-06-07 14:02:52 +0200

    3 commit

--- js/script.js
+++ js/script.js
@@ -8,19 +8,7 @@ productos = [
   ["Pimientos", 2, 1]
 ];
 
-var productsSelectors = [
-  document.querySelector("#Patatas"), document.querySelector("#Zanahorias")
-];
-
-// Reacciones Botones
-var addToCartButtons = document.querySelectorAll(".addCarrito");
-addToCartButtons.forEach((addToCart) => {
-  addToCart.addEventListener("click", () => {
-  console.log(productsSelectors);
-  });
-});
-
-//Creacion de tabla para el carrito
+// Creacion de tabla para el carrito
 var tabla = document.createElement("table");
 document.querySelector("#lista").appendChild(tabla);
 var indicadores = document.createElement("tr");
@@ -30,24 +18,43 @@ tabla.appendChild(indicadores);
 tabla.id = "tabla";
 tabla.style.display = "none"
 
+// Reacciones Botones
+var addToCartButtons = document.querySelectorAll(".addCarrito");
+addToCartButtons.forEach((addToCart) => {
+  addToCart.addEventListener("click", () => {
+    // Display ON de Indicadores
+    tabla.style.display = "table";
+    // Generador de la Tabla del Carrito 
+    for (let producto of productos) {
+      var tr = document.createElement("tr");
+      var ifExistData = document.querySelector(`#tr${producto[0]}`);
+      var productId = addToCart.closest("a").id;
+      if (productId.includes(producto[0])) {
+        if (ifExistData) {
+          ifExistData.innerHTML = `<td>${producto[0]}</td><td>${producto[1]}€</td><td>${producto[2] += 1}</td><td><button id="deleteItem" type="button" class="btn btn-danger">X</button></td>`;
+        } else {
+          tr.id = `tr${producto[0]}`;
+          tr.innerHTML = `<td>${producto[0]}</td><td>${producto[1]}€</td><td>${producto[2]}</td><td><button id="deleteItem" type="button" class="btn btn-danger">X</button></td>`;
+          tabla.appendChild(tr);
+        }
+      }
+    }
+  });
+});
 // Permiso para Drag
 function allowDrop(ev) {
     ev.preventDefault();
-}
-
+};
 // En el Drag
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-}
-
+};
 // En el Drop
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  console.log(data);
-
   // Display ON de Indicadores
-  tabla.style.display = "table"
+  tabla.style.display = "table";
   // Generador de la Tabla del Carrito 
   for (let producto of productos) {
     var tr = document.createElement("tr");
@@ -62,11 +69,11 @@ function drop(ev) {
       }
     }
   }
-/*
+  // Boton de Eliminar
   var deleteItem = document.querySelector("#deleteItem");
-  deleteItem.addEventListener("click", () => {
-    console.log(`deleted item ${data}`)
-    tabla.remove(tr);
-});
-*/
-};
+  deleteItem.addEventListener("click", (deleted) => {
+  });
+}
+
+
+
